784c0f9896cbbe033642d7db215701d54a1f521f
ACitronella <phuriwat.13042@gmail.com>
2024-08-26 16:19:13 +0700

    make code consistant between dataset

--- treato/trainpykeen.py
+++ treato/trainpykeen.py
@@ -1,38 +1,40 @@
-# from pykeen.triples import TriplesFactory
 import os
 import json
 from pykeen.pipeline import pipeline
 from util_eval_pykeen import plot_loss, get_metrics
 import pandas as pd
-# import torch_directml
 import torch
+
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(device)
 
-save_dir = 'pykeen_eval/test_pre_stratified/'
+save_dir = 'pykeen_eval/test2/'
 model_list = ["TransE", "ComplEx", "TransH", "DistMult", "ProjE"] # "ConvE"
-n_epochs = [200, 500, 1000]
+n_epochs = [200, 500, 1000, 2000, 5000]
 
 df = []
 
-for model in model_list:
+for model_name in model_list:
     for epochs in n_epochs:
-        save_path = os.path.join(save_dir, f"{model}{epochs}")
+        save_path = os.path.join(save_dir, f"{model_name}{epochs}")
         result = pipeline(
             training="data/train.tsv",
             validation="data/validate.tsv",
             testing="data/test.tsv",
-            model=model,
+            model=model_name,
             epochs=epochs,
             device=device
         )
         result.save_to_directory(save_path)
-
+        
         with open(os.path.join(save_path, "results.json"), "rb") as f:
             results = json.load(f)
-        plot_loss(results, save_path)
+        plot_loss(results["losses"], save_path)
         metrics = get_metrics(results)
-        metrics["model"] = model
+        for k, v in metrics.items():
+            if k.startswith("hits_at_"):
+                metrics[k] = v * 100
+        metrics["model"] = model_name
         metrics["epochs"] = epochs
 
         df.append(metrics)
